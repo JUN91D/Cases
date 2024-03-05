cf0a135f6d8187a0f19d50d0617de8cab9eb4810
Junaid Tahir <159828160+JUN91D@users.noreply.github.com>
2024-03-05 23:59:45 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+// camelCase
+// snake_case
+// PascalCase
+var userName = 'Junaid'; // e.g. camelCase
+var user_name = 'Maaz'; // e.g. snake_case
+var UserName = 'Chota Bheem'; // e.g. PascalCase
+var isUserExist = true;
+var is_user_exist = false;
+var IsUserExist = 1;
+// = is assignment operator
+// console.log(userName, user_name, UserName)
+// console.log(isUserExist, is_user_exist,IsUserExist)
+// Arrays => [] => 0, ...
+var students = ["Junaid", "Maaz", "Kamran", 1, 10, true];
+var firstStudentName = students[1];
+console.log(firstStudentName);
+console.log(students[5]);
+console.log(students[6]);
---
+++ main.ts
@@ -0,0 +1,25 @@
+// camelCase
+// snake_case
+// PascalCase
+
+let userName: string = 'Junaid' // e.g. camelCase
+let user_name: string = 'Maaz' // e.g. snake_case
+let UserName: string = 'Chota Bheem' // e.g. PascalCase
+
+let isUserExist: boolean = true;
+let is_user_exist: boolean = false;
+let IsUserExist: number = 1;
+ 
+// = is assignment operator
+
+// console.log(userName, user_name, UserName)
+// console.log(isUserExist, is_user_exist,IsUserExist)
+
+// Arrays => [] => 0, ...
+
+let students = ["Junaid", "Maaz", "Kamran", 1, 10, true]
+let shapatar = students[1]
+
+console.log(shapatar)
+console.log(students[5])
+console.log(students[6])
